19adc5b27414e8d5fcaaa7b6023b9aba362b430c
Sunil Kumar <sunilkumarsk8899@gmail.com>
2024-11-27 19:06:52 +0530

    Message insert in db

--- app/Livewire/ChatComponent.php
+++ app/Livewire/ChatComponent.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire;
 
+use App\Models\Message;
 use App\Models\User;
 use Livewire\Component;
 
 class ChatComponent extends Component
 {
     public $user;
+    public $sender_id;
+    public $receiver_id;
+    public $message = '';
 
     public function render()
     {
@@ -16,6 +20,18 @@ class ChatComponent extends Component
 
     public function mount($user_id){
         // dd($user_id);
+        $this->sender_id = auth()->user()->id;
+        $this->receiver_id = $user_id;
         $this->user = User::whereId($user_id)->first();
+    }
+
+    public function sendMessage(){
+        $message = new Message();
+        $message->sender_id = $this->sender_id;
+        $message->receiver_id = $this->receiver_id;
+        $message->message = $this->message;
+        $message->save();
+        $this->message = '';
+
     }
 }
