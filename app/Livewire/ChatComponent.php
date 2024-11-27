bd6f8c55de203d2a7222dad225e5e573236f7a8c
Sunil Kumar <sunilkumarsk8899@gmail.com>
2024-11-27 17:46:10 +0530

    Chat App

---
+++ app/Livewire/ChatComponent.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\User;
+use Livewire\Component;
+
+class ChatComponent extends Component
+{
+    public $user;
+
+    public function render()
+    {
+        return view('livewire.chat-component');
+    }
+
+    public function mount($user_id){
+        // dd($user_id);
+        $this->user = User::whereId($user_id)->first();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'welcome');
+
+Route::get('/dashboard', function(){
+    $users = User::where('id','!=',auth()->user()->id)->get();
+    return view('dashboard', compact('users'));
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+Route::get('/chat/{id}', function($id){
+    return view('chat',[
+        'id' => $id
+    ]);
+})->middleware(['auth', 'verified'])->name('chat');
+
+
+
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
